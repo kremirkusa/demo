59342a23c3a3f05f06dfebfa50464c6ed39c2675
kremirkusa <154733025+kremirkusa@users.noreply.github.com>
2023-12-30 21:22:08 +0100

    Update StudentRepository.java
    week 2 - query

--- demo/src/main/java/com/example/demo/student/StudentRepository.java
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,14 @@ public interface StudentRepository
 
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
+
+    List<Student> findByNameStartingWith(String name);
+
+    List<Student> findByEmailStartingWith(String email);
+
+    List<Student> findByDobBetween(LocalDate dobAfter, LocalDate dobBefore);
+
+    List<Student> findByDobAfter(LocalDate dobAfter);
+
+    List<Student> findByDobBefore(LocalDate dobBefore);
 }
