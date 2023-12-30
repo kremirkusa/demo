72728966187fc83fcbcdc68315cb13a9c09c0a5c
kremirkusa <154733025+kremirkusa@users.noreply.github.com>
2023-12-30 21:21:07 +0100

    Update StudentController.java
    week 2 query

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -19,10 +19,44 @@ public class StudentController {
 
 
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudent(
+        @RequestParam(required = false) Long id,
+                @RequestParam(required = false) String name,
+                @RequestParam(required = false) String email,
+                @RequestParam(required = false) String dobAfter,
+                @RequestParam(required = false) String dobBefore
+    ){
+        validateParameterCombination(id, name, email, dobAfter, dobBefore);
 
-        return studentService.getStudents();
+        if(id != null){
+            return studentService.getStudentById(id);
+        }
+        else if(name != null){
+            return studentService.getStudentByName(name);
+        }
+        else if(email != null){
+            return studentService.getStudentByEmail(email);
+        }
+        else if (dobAfter != null && dobBefore != null){
+            return studentService.getStudentsBornBetween(dobAfter, dobBefore);
+        }
+        else if (dobAfter != null) {
+            return studentService.getStudentsBornAfter(dobAfter);
+        } else if (dobBefore != null) {
+            return studentService.getStudentsBornBefore(dobBefore);
+        } else {
+            return studentService.getStudents();
+        }
     }
+
+    private void validateParameterCombination(Long id, String name, String email, String dobAfter, String dobBefore) {
+        if ((id != null && (name != null || email != null || dobAfter != null || dobBefore != null))
+                || (name != null && (email != null || dobAfter != null || dobBefore != null))
+                || (email != null && (dobAfter != null || dobBefore != null))) {
+            throw new IllegalArgumentException("Nevaljana kombinacija!");
+        }
+    }
+
 
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
